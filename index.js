4b349c5c6267d9a9fe139c86a250f22ada94dda4
lawgsy <lawgsy@gmail.com>
2017-07-12 20:38:17 +0200

    Cleanup

--- index.js
+++ index.js
@@ -4,41 +4,23 @@ const { spawn } = require('child_process');
 const fs    = require('fs');
 const path  = require('path');
 
-/**
- * Replace all occurences in given text.
- * @param  {string} text    Original text
- * @param  {string} search  Text to be replaced
- * @param  {string} replace Replacement text
- * @return {string}         Resulting text
- */
-function replaceAll(text, search, replace) {
-  return text.split(search).join(replace);
-}
-
-module.exports = (pluginContext) => {
-  const logger   = pluginContext.logger;
-  const toast    = pluginContext.toast;
-  const app      = pluginContext.app;
+module.exports = ({logger, toast, app}) => {
   const template = fs.readFileSync(path.join(__dirname, 'output.html'), 'utf8');
-  const DEBUG    = false;
   let timer      = null;
   let prev_cmd   = "";
-  let output     = "";
   let html       = "";
 
   function startup() {
-    html = replaceAll(template,'%output%', "Output goes here, if any...");
+    html = template.replace('%output%', "Output goes here, if any...");
   }
 
   function search (query, res) {
     const query_trim = query.trim();
-
-    // no input
     if(query_trim.length === 0) return;
 
     // new input (and not a forced refresh)
     if(query_trim != prev_cmd && query_trim != prev_cmd+'.')
-      html = replaceAll(template,'%output%', "Output goes here, if any...");
+      html = template.replace('%output%', "Output goes here, if any...");
 
     res.add({
       id:      query_trim,
@@ -47,11 +29,6 @@ module.exports = (pluginContext) => {
       desc:    `Execute <b>${query_trim}</b>`,
       preview: true
     });
-
-  }
-
-  function debug() {
-    if (DEBUG) logger.log('DEBUG::', arguments);
   }
 
   function execute(id, payload) {
@@ -60,37 +37,38 @@ module.exports = (pluginContext) => {
     let args  = id.split(' ');
     let cmd   = args.shift();
     let child = spawn(cmd, args);
-    output    = "";
+    let output    = "";
 
     timer = setTimeout(() => {
       child.kill();
       toast.enqueue(`'${cmd}' timed out (and was killed)`);
       output += "(execution interrupted - 5 second timeout reached)";
 
-      forceRefresh(id);
+      forceRefresh(id, output);
     }, 5000); // 5 second time-out by default
 
     toast.enqueue(`Spawned ${cmd} with arguments ['`+args.join('\', \'')+`']`);
 
     child.stdout.on('data', data => { // stdout received
       output += data;
-      forceRefresh(id);
+      forceRefresh(id, output);
     });
+
     child.stderr.on('data', data => { // stderr received
       output += `<span style="color:#FF0000">${data}</span>`;
-      forceRefresh(id);
+      forceRefresh(id, output);
     });
 
     child.on('close', code => { // execution finished or was interrupted
       clearTimeout(timer);
 
       output += `child process exited with code ${code}`;
-      forceRefresh(id);
+      forceRefresh(id, output);
     });
   }
 
-  function forceRefresh(id) {
-    html = replaceAll(template,'%output%', output);
+  function forceRefresh(id, output) {
+    html = template.replace('%output%', output);
 
     // hack to force preview refresh by setting a different query, then
     // setting it back to the user input
